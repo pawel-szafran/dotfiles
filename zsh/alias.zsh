48d46fdeb7825aa77a0fa0347a0166661692e006
Pawel Szafran <pawel.szafran@gmail.com>
2015-10-15 00:18:40 -0400

    Add update cmd and go back to prev dir

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,1 +1,2 @@
-alias preztou='cd ~/.zprezto; git pull && git submodule update --init --recursive; cd'
+alias preztou='cd ~/.zprezto; git pull && git submodule update --init --recursive; cd -'
+alias dfu='cd ~/.dotfiles; git pull; source install.zsh; cd -'
