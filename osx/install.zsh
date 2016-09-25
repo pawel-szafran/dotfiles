06b467988c2cf4bf66005315e8b1842b6d8d2168
Pawel Szafran <pawel.szafran@gmail.com>
2016-09-25 15:08:09 -0400

    Speed up Dock animation

--- osx/install.zsh
+++ osx/install.zsh
@@ -12,6 +12,10 @@ defaults write com.apple.screencapture disable-shadow -bool true
 # Speed up Mission Control animations
 defaults write com.apple.dock expose-animation-duration -float 0.15
 
+# Speed up Dock animation
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0.4
+
 # Google Chrome
 defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
 defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -bool false
