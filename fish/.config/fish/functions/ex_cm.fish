c2e8de650fd8607be0cd0c80f4f13d69c3e9941d
Pawel Szafran <pawel.szafran@gmail.com>
2023-08-29 21:26:45 +0200

    fish: improve Elixir funs

---
+++ fish/.config/fish/functions/ex_cm.fish
@@ -0,0 +1,7 @@
+##
+## Elixir Codemods
+##
+
+function ex_cm
+  xh -b ":1887/$(string join '/' $argv)"
+end
--- fish/.config/fish/functions/ex_s.fish
+++ fish/.config/fish/functions/ex_s.fish
@@ -1,20 +1,24 @@
 ##
-## Snippets
+## Elixir Snippets
 ##
 
-function s
+function ex_s
   switch $argv[1]
     case i
-      while read line
+      while read -l line
         echo -n "|> IO.inspect(label: ~s<$line>)"
       end
     case ip
-      while read line
+      while read -l line
         echo -n "|> IO.inspect(label: ~s<$line>, pretty: true)"
       end
     case if
-      while read line
+      while read -l line
         echo -n "|> IO.inspect(label: ~s<$line>, pretty: true, limit: :infinity, printable_limit: :infinity)"
+      end
+    case ifr
+      while read -l line
+        echo -n "|> IO.inspect(label: ~s<$line>, pretty: true, limit: :infinity, printable_limit: :infinity, structs: false)"
       end
   end
 end
