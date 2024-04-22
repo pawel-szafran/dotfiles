1905c9fca5c94ba2e979a1421c2738fd406129ae
Pawel Szafran <pawel.szafran@gmail.com>
2024-04-22 23:10:44 +0200

    hx: clean up gh menu

--- fish/dot-config/fish/functions/hx_gh_browse.fish
+++ fish/dot-config/fish/functions/hx_gh_browse.fish
@@ -4,6 +4,12 @@
 
 function hx_gh_browse
     set -f file (string replace "$PWD/" '' $argv[1])
-    set -f branch (git rev-parse --abbrev-ref HEAD)
+
+    if set -q argv[2]
+        set -f branch $argv[2]
+    else
+        set -f branch (git rev-parse --abbrev-ref HEAD)
+    end
+
     gh browse -b $branch $file
 end
