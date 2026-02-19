12df613b6ae37b1508b72e7344fb1a0db1235a90
Pawel Szafran <pawel.szafran@gmail.com>
2026-02-19 23:35:07 +0100

    tmux: reorg killing

--- tmux/dot-config/tmux/scripts/kill-sessions.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$SCRIPT_DIR/lib/session.sh"
-
-tmux list-sessions -F "#{session_name}" | fzf --multi --reverse --header "Kill sessions (Tab to multi-select)" --header-first | while read session; do
-    kill_session_with_teardown "$session"
-done
