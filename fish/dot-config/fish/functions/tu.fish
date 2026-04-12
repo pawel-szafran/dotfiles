dfb16b9ab880197c4b4c0950c30a23f42e7a9e8d
Pawel Szafran <pawel.szafran@gmail.com>
2026-04-12 17:48:46 +0200

    mise: simplify latest elixir

--- fish/dot-config/fish/functions/tu.fish
+++ fish/dot-config/fish/functions/tu.fish
@@ -14,17 +14,10 @@ function tu -d "Update terminal setup"
 
     mise plugins update
 
-    for tool in erlang rust go node python lua-language-server zig
+    for tool in erlang elixir rust go node python lua-language-server zig
         mise install -y {$tool}@latest
         mise use -g --pin {$tool}@latest
     end
-
-    # Elixir latest has no OTP ver, e.g. 1.18.4-otp-, so we need extra handling:
-    set -l otp (string split -f1 . (mise latest erlang))
-    set -l elixir_ver (string trim (mise latest elixir))
-    set -l elixir_full_ver $elixir_ver$otp
-    mise install -y elixir@$elixir_full_ver
-    mise use -g --pin elixir@$elixir_full_ver
 
     tu_log "Updating cargo"
     for pkg in amber ccase
