97c736235cba8b08bb4e1f973feba906ab622303
Pawel Szafran <pawel.szafran@gmail.com>
2026-05-09 11:15:20 +0200

    tu: shorten log for symlinking

--- fish/dot-config/fish/functions/tu_sym.fish
+++ fish/dot-config/fish/functions/tu_sym.fish
@@ -1,8 +1,8 @@
 function tu_sym -d "Update terminal setup symlinks"
     cd $DOTFILES_DIR
 
+    tu_log Symlinking
     for pkg in stow fish ghostty git glow helix tmux lazygit btop yazi worktrunk
-        tu_log "Symlinking $pkg"
         stow -v -t $HOME --no-folding --dotfiles $pkg
     end
 
