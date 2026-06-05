7d746629d980cb045263e2e722d3efd72e5dd5d7
Pawel Szafran <pawel.szafran@gmail.com>
2026-06-05 11:04:37 +0200

    lazydocker: add config

--- fish/dot-config/fish/functions/tu_sym.fish
+++ fish/dot-config/fish/functions/tu_sym.fish
@@ -2,7 +2,7 @@ function tu_sym -d "Update terminal setup symlinks"
     cd $DOTFILES_DIR
 
     tu_log Symlinking
-    for pkg in stow fish ghostty git glow helix tmux lazygit btop yazi worktrunk
+    for pkg in stow fish ghostty git glow helix tmux lazygit btop yazi worktrunk lazydocker
         stow -v -t $HOME --no-folding --dotfiles $pkg
     end
 
